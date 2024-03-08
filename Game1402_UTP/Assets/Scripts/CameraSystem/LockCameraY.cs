cdabcad4c3408096ea67d150f8b705316bfb1f26
Suellen S <suellendesalles@gmail.com>
2024-03-07 18:30:16 -0700

    Refactoring LockCameraY script

--- Game1402_UTP/Assets/Scripts/CameraSystem/LockCameraY.cs
+++ Game1402_UTP/Assets/Scripts/CameraSystem/LockCameraY.cs
@@ -5,7 +5,7 @@ using Cinemachine;
 public class LockCameraY : CinemachineExtension
 {
     [SerializeField]
-    float m_YPosition = 13;
+    float yPosition = 13;
 
     protected override void PostPipelineStageCallback(
         CinemachineVirtualCameraBase vcam,
@@ -13,8 +13,8 @@ public class LockCameraY : CinemachineExtension
     {
         if (stage == CinemachineCore.Stage.Body)
         {
-            var position = state.RawPosition;
-            position.y = m_YPosition;
+            Vector3 position = state.RawPosition;
+            position.y = yPosition;
             state.RawPosition = position;
         }
     }
