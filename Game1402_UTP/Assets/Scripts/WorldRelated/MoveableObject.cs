c539b86230ab28814fc467f3d7d350a70a902c26
Suellen S <suellendesalles@gmail.com>
2024-03-07 18:28:00 -0700

    Implementing move objects ability by attaching MoveableObject script to the player

---
+++ Game1402_UTP/Assets/Scripts/WorldRelated/MoveableObject.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableObject : MonoBehaviour
+{
+    [SerializeField]
+    float pushForce = 1;
+
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        Debug.Log("name:" + hit.gameObject.name);
+
+        #region approach 1
+        // no rigidbody  
+        if (body == null || body.isKinematic)
+            return;
+
+        // We dont want to push objects below us
+        if (hit.moveDirection.y < -0.3f)
+            return;
+
+        // Calculate push direction from move direction,
+        // we only push objects to the sides never up and down
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+        // Apply the push
+        body.velocity = pushDir * pushForce;
+        #endregion
+
+
+        #region approach 2
+        /*
+        if (body != null)
+        {
+            // move to one direction
+            Vector3 forceDirection = hit.gameObject.transform.position - transform.position;
+            forceDirection.y = 0; //do not move on y axis
+            forceDirection.Normalize();
+
+            body.AddForceAtPosition(forceDirection * pushForce, transform.position, ForceMode.Impulse);
+        }
+        */
+        #endregion
+    }
+}
