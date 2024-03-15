b604e75cbbd193186d6eee3a34ae8c3c721b708e
Suellen S <suellendesalles@gmail.com>
2024-03-15 00:00:31 -0600

    Adding RigidBody and Collider to the Box prefab. Adding Main Menu and Pause Menu. Adding GameManager script. Adding rotator enemy prefab.

--- Game1402_UTP/Assets/Scripts/PlayerRelated/PlayerInputManager.cs
+++ Game1402_UTP/Assets/Scripts/PlayerRelated/PlayerInputManager.cs
@@ -11,8 +11,8 @@ public class PlayerInputManager : MonoBehaviour
     void Awake()
     {
         _playerController = GetComponent<PlayerController>();
-        _pauseMenu = FindObjectOfType<PauseMenu>();
-    }
+        _pauseMenu = FindObjectOfType<PauseMenu>();
+    }
     void OnEnable()
     {
         if (_inputActions == null)
@@ -22,7 +22,7 @@ public class PlayerInputManager : MonoBehaviour
             _inputActions.PlayerAction.Attack.performed += i => _playerController.ProcessAttack(true);
             _inputActions.PlayerAction.Interact.performed += i => _playerController.Interaction();
             _inputActions.PlayerAction.Dodge.performed += i => _playerController.ProcessDodge(true);
-            _inputActions.PlayerAction.Pause.performed += i => _pauseMenu.HandlePauseMenu();
+            _inputActions.PlayerAction.Pause.performed += i => _pauseMenu.HandlePauseMenu();
         }
         _inputActions.Enable();
     }
